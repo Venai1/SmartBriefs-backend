ea730704d70e8c231d94cf4e58a4ec6df1ab724a
Venai Seepersaud <venai.seepersaud@gmail.com>
2025-03-08 17:19:24 -0500

    trying docker6

--- helperFunctions/create_account_transactions/create_random_withdrawal.py
+++ helperFunctions/create_account_transactions/create_random_withdrawal.py
@@ -23,7 +23,7 @@ def create_random_withdrawal(account_id):
         ])
     }
 
-    url = f"{os.getenv("NESSIE_API_URL")}/accounts/{account_id}/withdrawals?key={os.getenv("NESSIE_API_KEY")}"
+    url = f"{os.getenv("NESSIE_API_URL")}/accounts/{account_id}/withdrawals?key={os.getenv('NESSIE_API_KEY')}"
 
     headers = {
         "Content-Type": "application/json",
--- helperFunctions/get_accounts_for_customer.py
+++ helperFunctions/get_accounts_for_customer.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 def get_accounts_for_customer(customer_id):
     """Get all accounts for a customer"""
     load_dotenv()
-    url = f"{os.getenv("NESSIE_API_URL")}/customers/{customer_id}/accounts?key={os.getenv("NESSIE_API_KEY")}"
+    url = f"{os.getenv('NESSIE_API_KEY')}/customers/{customer_id}/accounts?key={os.getenv('NESSIE_API_KEY')}"
     try:
         response = requests.get(url)
         response.raise_for_status()
